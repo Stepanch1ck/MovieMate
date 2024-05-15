d9297a7d2758c66c361f06162e3038d2362c6bd5
Stepan Ch <noname.churkin@gmail.com>
2024-05-15 18:37:12 +0300

    .

--- MovieMate/AfterEnterForms/CompilationForm/addMovie.cs
+++ MovieMate/AfterEnterForms/CompilationForm/addMovie.cs
@@ -1,10 +1,12 @@
 ﻿using MovieMate.DBConnect;
-using System;
+using NLog;
 
 namespace MovieMate.AfterEnterForms
 {
     public partial class addMovie : Form
     {
+
+        private static readonly Logger logger = LogManager.GetCurrentClassLogger();
         public string selectedMovie = string.Empty;
         MovieDbContext db = new MovieDbContext();
         Compilation currentCompilation;
@@ -13,53 +15,78 @@ namespace MovieMate.AfterEnterForms
         {
             InitializeComponent();
             currentCompilation = compilation;
-            MovieListBox.Items.Clear();
-            foreach (var movie in db.Movies.ToList())
+            try
             {
-                MovieListBox.Items.Add(movie.Name);
+                MovieListBox.Items.Clear();
+                foreach (var movie in db.Movies.ToList())
+                {
+                    MovieListBox.Items.Add(movie.Name);
+                }
+                MovieListBox.Visible = true;
             }
-            MovieListBox.Visible = true;
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Ошибка при загрузке списка фильмов.");
+                MessageBox.Show("Произошла ошибка при загрузке списка фильмов.");
+            }
         }
 
         private void movieAddButton_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(selectedMovie))
+            try
             {
-                var selectedmovie = db.Movies.FirstOrDefault(p => p.Name == selectedMovie);
-
-                if (selectedmovie != null)
+                if (!string.IsNullOrEmpty(selectedMovie))
                 {
-                    if (currentCompilation.IdMovie.Contains(selectedmovie.Id.ToString()))
+                    var selectedmovie = db.Movies.FirstOrDefault(p => p.Name == selectedMovie);
+
+                    if (selectedmovie != null)
                     {
-                        MessageBox.Show("Этот фильм уже есть в подборке");
-                        return;
-                    }
-                    if (string.IsNullOrEmpty(currentCompilation.IdMovie))
-                    {
-                        currentCompilation.IdMovie = selectedmovie.Id.ToString();
+                        if (currentCompilation.IdMovie.Contains(selectedmovie.Id.ToString()))
+                        {
+                            MessageBox.Show("Этот фильм уже есть в подборке");
+                            return;
+                        }
+
+                        if (string.IsNullOrEmpty(currentCompilation.IdMovie))
+                        {
+                            currentCompilation.IdMovie = selectedmovie.Id.ToString();
+                        }
+                        else
+                        {
+                            currentCompilation.IdMovie += "," + selectedmovie.Id;
+                        }
+
+                        db.SaveChanges();
+                        logger.Info($"{selectedMovie} добавлен в подборку!");
+                        MessageBox.Show($"{selectedMovie} добавлен в подборку!");
+                        this.Close();
                     }
                     else
                     {
-                        currentCompilation.IdMovie += "," + selectedmovie.Id;
+                        MessageBox.Show("Фильм не найден.");
                     }
-
-                    db.SaveChanges();
-                    MessageBox.Show($"{selectedMovie} добавлен в подборку!");
-
-                    this.Close();
                 }
-                else
-                {
-                    MessageBox.Show("Фильм не найден.");
-                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Ошибка при добавлении фильма в подборку.");
+                MessageBox.Show("Произошла ошибка при добавлении фильма в подборку.");
             }
         }
 
         private void MovieListBox_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (MovieListBox.SelectedItems.Count > 0)
+            try
             {
-                selectedMovie = MovieListBox.SelectedItem.ToString();
+                if (MovieListBox.SelectedItems.Count > 0)
+                {
+                    selectedMovie = MovieListBox.SelectedItem.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex, "Ошибка при выборе фильма из списка.");
+                MessageBox.Show("Произошла ошибка при выборе фильма из списка.");
             }
         }
     }
